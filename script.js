8599e399b59f28d216cf39cffa563d29fef7bd15
Irina <irenserge@mail.ru>
2026-09-25 15:03:43 +0300

    feat: add json, tabs & start modal window

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+// render cards
+let allProducts = [];
+const wrapperEl = document.querySelector('.coffee__wrapper');
+const tabsEls = document.querySelectorAll('.tab');
+
+async function getData() {
+    const res = await fetch('./products.json');
+    return res.json();
+}
+
+async function init() {
+    allProducts = await getData();
+    renderCards('coffee');
+}
+
+function renderCards(cat) {
+    const fileredProds = allProducts.filter(item => item.category === cat);
+    if (!wrapperEl) return;
+    wrapperEl.innerHTML = '';
+
+    fileredProds.forEach((prod, index) => {
+        const card = document.createElement('div');
+        card.className = 'coffee__card';
+        card.innerHTML = `
+            <img src="./images/${prod.category}/${prod.category}-${index + 1}.png" alt=${prod.name} class="coffee__card-img">
+               <div class="card__info">
+                    <div class="card__descr">
+                        <h2 class="coffee__card-title">${prod.name}</h2>
+                        <p class="coffee__card-text">${prod.description} </p>
+                    </div>
+                    <p class="coffee__card-price">$${prod.price}</p>
+               </div>
+        `;
+        wrapperEl.appendChild(card);
+    })
+}
+init();
+
+tabsEls && tabsEls.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        tabsEls.forEach(el => el.classList.remove('active'));
+        e.currentTarget.classList.add('active');
+        renderCards(e.currentTarget.id);
+    })
+});
+
+// modal window
+
+wrapperEl.addEventListener('click', (e) => {
+    console.log(e.target.closest('.coffee__card'));
+});
+
+function showModal () {
+    
+}
+
+// menu burger
+
+
+// pagination
+
+
+//slider
+
+
+// theme
+
 (function () {
     const STORAGE_KEY = 'coffee-theme';
     const body = document.body;
